6d7d8b22e2060e8470339fdf046cd894d47779a1
Dariya Gecher <dariya.gecher@gmail.com>
2023-05-05 23:05:56 +0600

    Initial Commit

---
+++ myList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myList
+//
+//  Created by Dariya Gecher on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
