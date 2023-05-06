b62ea1120c0264ae6560aee835d87c1647262aec
Dariya Gecher <dariya.gecher@gmail.com>
2023-05-06 23:53:54 +0600

    studied base of tableview manipulation methods

--- myList/TableViewController.swift
+++ myList/TableViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    let array = [1, 2, 3, 4, 5]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,23 +25,29 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return array.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
         // Configure the cell...
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = "\(array[indexPath.row])"
 
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 89
+    }
+    
 
     /*
     // Override to support conditional editing of the table view.
